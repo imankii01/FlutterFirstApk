0e213d11b9f6cf3b5ddcb1ac00793dd17eca246a
Ankit <codewithankit047@gmail.com>
2024-03-16 23:04:38 +0530

    class and object

--- lib/practise.dart
+++ lib/practise.dart
@@ -6,11 +6,19 @@ myFunc();
 void myFunc (){
   print("welcome to func");
   var cm = myClass();
-  cm.printName();
+  //  cm.printName();
+   myClass().printName("adfjha");
+   myClass().printName("adffdfgsfgsgjha");
+   myClass().printName("sfgsgfsdfgsgf");
+   myClass().printName("adfwrtwertwtrjha");
+  print( myClass().addFunc(5,6));
 
 }
 class myClass{
-  void printName(){
-     print("Ankit Tech"); //Decleatrion
+  void printName(String name){
+     print("Ankit Tech,$name"); //Decleatrion
+  }
+  int addFunc(int a,int b){
+    return a+b;
   }
 }
