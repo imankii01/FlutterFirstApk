e1b6b962cdeba48b49f0391af392f901cc1c95bb
Ankit <codewithankit047@gmail.com>
2024-03-16 22:42:14 +0530

    fucntion

--- lib/practise.dart
+++ lib/practise.dart
@@ -1,87 +1,16 @@
 import 'dart:io';
 
-void main() {
-
-//   print("hello world");
-//   stdout.write("Enter your Name: ");
-//   var name = stdin.readLineSync();
-//   print("Hello, $name!");
-//   var names ="Ankit";
-//   var year = 139;
-//   var radius = 4.5;
-//   var flyobjects =['cturn','sadf'];
-//   var image ={
-//     'tags':['x','d'],
-//    'url':"//path/to/saturn.jpg"
-//   };
-//   print("$names,$year,$radius,$flyobjects,$image");
-//   if(year > 5){
-//     print("yes");
-//   }else{
-//     print("no");
-//   }
-//   for (var object in flyobjects) {
-//     print(object);
-    
-//   }
-//   for(int month = 1;month <=12;month++){
-//     print(month);
-//   }
-// while(year  <2016){
-//   year +=1;
-//   print(year);
-// }
-// var result = fibonacci(20);r
-// print(result);
-// flyobjects.where((name) => name.contains('turn')).forEach(print);
-
-// int? a=5;    //intilsation
-// print(a);
-// a=7;
-// print(a);
-// // to print null
-// int ? b;
-// print(b);
-// // inline declaration
-// String name ='Ankit';
-// print(name);
-// BigInt longValue ;
-// longValue =BigInt.parse('234798572345927345973249723479324597345972345879324893497');
-// print(longValue);
-// double percentage = 99.65;
-// num percentagesss = 99.65;
-// bool isCheck  = false;
-
-// Roll of var and dynamic
-// String a ="Ankit";
-
-
-// // var
-// var subject = 'Maths';
-// subject = 7 as String; //not allot due to typecast differtn ,& by using as its chagne the ype cast
-// var number  = 455;
-// number  = 'asdfjh' as int;
-// print(subject);
-// var section;
-dynamic section;
-section = 44534;
-print(section);
-section = 'dgdfs3';
-print(section);
-
+void main(){
+myFunc();
+}
+void myFunc (){
+  print("welcome to func");
+  var cm = myClass();
+  cm.printName();
 
 }
-
-
-int fibonacci(int n) {
-  if (n == 0 || n == 1) return n;
-  return fibonacci(n - 1) + fibonacci(n - 2);
-
-  var raman = new Human();
-     
-}
-
-class Human {
-  Human();
-
+class myClass{
+  void printName(){
+     print("Ankit Tech"); //Decleatrion
+  }
 }
