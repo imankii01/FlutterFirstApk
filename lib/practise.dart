a1a519507a1ed76db6ee6c5a1484e09cb5e14fa6
Ankit <codewithankit047@gmail.com>
2024-03-16 14:18:15 +0530

    data types

--- lib/practise.dart
+++ lib/practise.dart
@@ -2,42 +2,69 @@ import 'dart:io';
 
 void main() {
 
-  print("hello world");
-  stdout.write("Enter your Name: ");
-  var name = stdin.readLineSync();
-  print("Hello, $name!");
-  var names ="Ankit";
-  var year = 139;
-  var radius = 4.5;
-  var flyobjects =['c','sadf'];
-  var image ={
-    'tags':['x','d'],
-   'url':"//path/to/saturn.jpg"
-  };
-  print("$names,$year,$radius,$flyobjects,$image");
-  if(year > 5){
-    print("yes");
-  }else{
-    print("no");
-  }
-  for (var object in flyobjects) {
-    print(object);
+//   print("hello world");
+//   stdout.write("Enter your Name: ");
+//   var name = stdin.readLineSync();
+//   print("Hello, $name!");
+//   var names ="Ankit";
+//   var year = 139;
+//   var radius = 4.5;
+//   var flyobjects =['cturn','sadf'];
+//   var image ={
+//     'tags':['x','d'],
+//    'url':"//path/to/saturn.jpg"
+//   };
+//   print("$names,$year,$radius,$flyobjects,$image");
+//   if(year > 5){
+//     print("yes");
+//   }else{
+//     print("no");
+//   }
+//   for (var object in flyobjects) {
+//     print(object);
     
-  }
-  for(int month = 1;month <=12;month++){
-    print(month);
-  }
-while(year  <2016){
-  year +=1;
-  print(year);
-}
-var result = fibonacci(20);
-print(result);
+//   }
+//   for(int month = 1;month <=12;month++){
+//     print(month);
+//   }
+// while(year  <2016){
+//   year +=1;
+//   print(year);
+// }
+// var result = fibonacci(20);r
+// print(result);
+// flyobjects.where((name) => name.contains('turn')).forEach(print);
+
+int? a=5;    //intilsation
+print(a);
+a=7;
+print(a);
+// to print null
+int ? b;
+print(b);
+// inline declaration
+String name ='Ankit';
+print(name);
+BigInt longValue ;
+longValue =BigInt.parse('234798572345927345973249723479324597345972345879324893497');
+print(longValue);
+double percentage = 99.65;
+num percentagesss = 99.65;
+bool isCheck  = false;
+
+
 }
 
 
 int fibonacci(int n) {
   if (n == 0 || n == 1) return n;
   return fibonacci(n - 1) + fibonacci(n - 2);
+
+  var raman = new Human();
+     
 }
 
+class Human {
+  Human();
+
+}
