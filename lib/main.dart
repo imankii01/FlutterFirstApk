da1300b8f21ba892b2d9383522def4e2d7dacf62
Ankit <codewithankit047@gmail.com>
2024-03-16 12:41:20 +0530

    tut 18 compelte

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Click here to send the message:',
+              'Click here to send the messages:',
             ),
             Text(
               '$_counter',
