f38a09487347cd377c3a58706fde091fdde793ec
Ankit <codewithankit047@gmail.com>
2024-03-17 14:08:29 +0530

    final & const

--- lib/practise.dart
+++ lib/practise.dart
@@ -1,27 +1,9 @@
 import 'dart:io';
 
- void main(){
-  // var list1 =[1,2,3,4,5,6,7,8];
-  // // print(list1);
-  // // for(final object in list1){
-  // //   print(object);
-  // //   if(object == 1){
-  // //     print("done");
-  // //   }
-  // // }
-  // var list2 =[];
-  // // list2.addAll(list1);
-  // // for(int object in list2){
-  // //   print(object);
-  // // }
-  // list2.insert(0, 200);
-  // list2.insert(1, 200);
-  // list2.insert(2, 200);
-  var map_name ={
-    'name':"ankit",
-    'class':'rollno',
-    'sub':"cSE"
-  };
-  map_name['name']='rohan';
-  print(map_name);
- }
+void main() {
+// final
+  // final name = 'ankit';
+  // name='dhsjfd';
+  const name ='rahul';
+  print(name);
+}
