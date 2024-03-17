3143478fc3d08d404e9f4138363a1be3adc5eab3
Ankit <codewithankit047@gmail.com>
2024-03-17 14:05:24 +0530

    map implementation

--- lib/practise.dart
+++ lib/practise.dart
@@ -1,24 +1,27 @@
 import 'dart:io';
 
-void main(){
-myFunc();
-}
-void myFunc (){
-  print("welcome to func");
-  var cm = myClass();
-  //  cm.printName();
-   myClass().printName("adfjha");
-   myClass().printName("adffdfgsfgsgjha");
-   myClass().printName("sfgsgfsdfgsgf");
-   myClass().printName("adfwrtwertwtrjha");
-  print( myClass().addFunc(5,6));
-
-}
-class myClass{
-  void printName(String name){
-     print("Ankit Tech,$name"); //Decleatrion
-  }
-  int addFunc(int a,int b){
-    return a+b;
-  }
-}
+ void main(){
+  // var list1 =[1,2,3,4,5,6,7,8];
+  // // print(list1);
+  // // for(final object in list1){
+  // //   print(object);
+  // //   if(object == 1){
+  // //     print("done");
+  // //   }
+  // // }
+  // var list2 =[];
+  // // list2.addAll(list1);
+  // // for(int object in list2){
+  // //   print(object);
+  // // }
+  // list2.insert(0, 200);
+  // list2.insert(1, 200);
+  // list2.insert(2, 200);
+  var map_name ={
+    'name':"ankit",
+    'class':'rollno',
+    'sub':"cSE"
+  };
+  map_name['name']='rohan';
+  print(map_name);
+ }
